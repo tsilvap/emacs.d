2f28b5cfa4a239d12d9354d60499be18b3282c62
Thiago da Silva <thiagodasilva@protonmail.com>
2023-03-27 06:14:58 -0300

    Add create-adhoc-directory function

--- lisp/tsp.el
+++ lisp/tsp.el
@@ -16,5 +16,14 @@
       (format "Run %s on region if the region is active, otherwise run it on buffer.\n\nThis is a generated function." formatter))
     (define-key keymap (kbd "C-c f") format-region-or-buffer-function)))
 
+(defun tsp/create-adhoc-directory ()
+  "Create adhoc directory to place files, scripts, etc."
+  (interactive)
+  (let ((adhoc-directory-name
+         (concat "~/Misc/adhoc/"
+                 (format-time-string "%Y-%m-%d-%H%M%S" (current-time)))))
+    (make-directory adhoc-directory-name t)
+    (find-file adhoc-directory-name)))
+
 (provide 'tsp)
 ;;; tsp.el ends here
