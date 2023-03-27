9f1244fb704c99f3aa4199052e29f52b948cbba0
Thiago da Silva <thiagodasilva@protonmail.com>
2023-03-27 09:44:21 -0300

    Bind dedicated Cape completion commands

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -18,5 +18,24 @@
 (dolist (cape-func '(cape-file cape-dabbrev))
   (add-to-list 'completion-at-point-functions cape-func t))
 
+;; Bind dedicated completion commands.
+(dolist (cmd '(("C-c p p" . completion-at-point)
+               ("C-c p t" . complete-tag)
+               ("C-c p d" . cape-dabbrev)
+               ("C-c p h" . cape-history)
+               ("C-c p f" . cape-file)
+               ("C-c p k" . cape-keyword)
+               ("C-c p s" . cape-symbol)
+               ("C-c p a" . cape-abbrev)
+               ("C-c p i" . cape-ispell)
+               ("C-c p l" . cape-line)
+               ("C-c p w" . cape-dict)
+               ("C-c p \\" . cape-tex)
+               ("C-c p _" . cape-tex)
+               ("C-c p ^" . cape-tex)
+               ("C-c p &" . cape-sgml)
+               ("C-c p r" . cape-rfc1345)))
+  (global-set-key (kbd (car cmd)) (cdr cmd)))
+
 (provide 'init-completion)
 ;;; init-completion.el ends here
