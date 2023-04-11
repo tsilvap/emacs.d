17a480dc1475eb526048bbe510b2842834ec0562
Thiago da Silva <thiagodasilva@protonmail.com>
2023-04-11 07:03:25 -0300

    Refactor init-perl.el

--- lisp/init-perl.el
+++ lisp/init-perl.el
@@ -2,27 +2,28 @@
 ;;; Commentary:
 ;;; Code:
 
+(require 'reformatter)
+(require 'tsp)
+
 ;; Prefer `cperl-mode' over `perl-mode'.
-(defalias 'perl-mode 'cperl-mode)
+(if (boundp 'major-mode-remap-alist)
+    (add-to-list 'major-mode-remap-alist '(perl-mode . cperl-mode))
+  (defalias 'perl-mode 'cperl-mode))
 
-(with-eval-after-load 'cperl-mode
-  (let ((indent-level 4))
-    (setq cperl-close-paren-offset (- indent-level)
-	  cperl-continued-statement-offset indent-level
-	  cperl-indent-level indent-level
-	  cperl-indent-parens-as-block t
-	  cperl-invalid-face 'default))
-
-  ;; Formatting
-  (require 'reformatter)
-  (require 'tsp)
-
-  (reformatter-define perltidy :program "perltidy" :args '("-" "--standard-output"))
-  (tsp/create-binding-for-formatter "perltidy" cperl-mode-map)
-
+(use-package cperl-mode
   ;; Remap `cperl-indent-command' to `indent-for-tab-command', which
   ;; respects the value of `tab-always-indent'.
-  (define-key cperl-mode-map [remap cperl-indent-command] #'indent-for-tab-command))
+  :bind ([remap cperl-indent-command] . indent-for-tab-command)
+  :config
+  (let ((indent-level 4))
+    (setq cperl-close-paren-offset (- indent-level)
+          cperl-indent-level indent-level
+          cperl-indent-parens-as-block t
+          cperl-invalid-face 'default))
+
+  ;; Create binding for running perltidy.
+  (reformatter-define perltidy :program "perltidy" :args '("-" "--standard-output"))
+  (tsp/create-binding-for-formatter "perltidy" cperl-mode-map))
 
 (provide 'init-perl)
 ;;; init-perl.el ends here
