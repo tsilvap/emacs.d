f2665f957dcaef62c539a71a33d1b66d90af9f1b
Thiago da Silva <thiagodasilva@protonmail.com>
2023-05-03 04:09:47 -0300

    Add a few more bindings to Go mode

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -5,6 +5,9 @@
 (setup go-mode
   (:bind
    "C-c f" eglot-format-buffer
+   "C-c l d" godoc
+   "C-c l r" eglot-rename
+
    ;; We use this for fixing the imports since gopls doesn't seem to
    ;; do that.
    "C-c l i" goimports)
