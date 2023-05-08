159f28e837a9cdd9ef8ad0c80e02b59246099d51
Thiago da Silva <thiagodasilva@protonmail.com>
2023-05-08 01:14:26 -0300

    Configure Gnus variables after load

--- lisp/init-gnus.el
+++ lisp/init-gnus.el
@@ -8,26 +8,27 @@
 ;;; Code:
 
 (setup gnus
-  (:option gnus-select-method '(nnimap "personal-mail"
-                                       (nnimap-address "127.0.0.1")
-                                       (nnimap-server-port 1144)
-                                       (nnimap-stream plain))
-           gnus-secondary-select-methods '((nntp "news.gmane.io")))
+  (:when-loaded
+    (:option gnus-select-method '(nnimap "personal-mail"
+                                         (nnimap-address "127.0.0.1")
+                                         (nnimap-server-port 1144)
+                                         (nnimap-stream plain))
+             gnus-secondary-select-methods '((nntp "news.gmane.io")))
 
-  ;; Sync Gnus directory and startup files.
-  (:option gnus-directory "~/Dropbox/News"
-           gnus-startup-file "~/Dropbox/.newsrc")
+    ;; Sync Gnus directory and startup files.
+    (:option gnus-directory "~/Dropbox/News"
+             gnus-startup-file "~/Dropbox/.newsrc")
 
-  ;; Render HTML emails with gnus-w3m.
-  (:option mm-text-html-renderer 'gnus-w3m)
+    ;; Render HTML emails with gnus-w3m.
+    (:option mm-text-html-renderer 'gnus-w3m)
 
-  ;; Send email via SMTP.
-  (:option message-send-mail-function 'smtpmail-send-it
-           smtpmail-smtp-server "127.0.0.1"
-           smtpmail-smtp-service 1026)
+    ;; Send email via SMTP.
+    (:option message-send-mail-function 'smtpmail-send-it
+             smtpmail-smtp-server "127.0.0.1"
+             smtpmail-smtp-service 1026)
 
-  ;; Initialize BBDB for Gnus and Message mode.
-  (bbdb-initialize 'gnus 'message))
+    ;; Initialize BBDB for Gnus and Message mode.
+    (bbdb-initialize 'gnus 'message)))
 
 (provide 'init-gnus)
 ;;; init-gnus.el ends here
