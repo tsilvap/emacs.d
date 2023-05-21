2a978a7e9f527445640fe2b09a3cc29b7e9ab45b
Thiago da Silva <thiagodasilva@protonmail.com>
2023-05-21 20:02:02 -0300

    Move backups configuration into init-files.el

--- lisp/init-defaults.el
+++ lisp/init-defaults.el
@@ -23,29 +23,6 @@
    ;; Ask for confirmation before closing Emacs.
    confirm-kill-emacs 'y-or-n-p)
 
-  ;; Make backup files (Emacs does this by default), with some better
-  ;; behavior.
-  (:option
-   make-backup-files t
-
-   ;; Create backups by copying, to avoid changing the owner/group of
-   ;; the file, and to avoid making soft/hard links point to the
-   ;; backup file.
-   backup-by-copying t
-
-   ;; Keep all backup files in a single directory, rather than
-   ;; littering the filesystem. Put Tramp backup files in that
-   ;; directory too.
-   backup-directory-alist `(("." . ,(concat user-emacs-directory "backups")))
-   tramp-backup-directory-alist backup-directory-alist
-
-   ;; Make numbered backup files, and delete excess backup files
-   ;; silenty.
-   version-control t
-   kept-new-versions 5
-   kept-old-versions 5
-   delete-old-versions t)
-
   ;; Make UTF-8 the default coding system.
   (set-language-environment "UTF-8"))
 
