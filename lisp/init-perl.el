6377407300cf7fc16d58e1d1677d0bcc239e2eb8
Thiago da Silva <thiagodasilva@protonmail.com>
2023-09-08 07:08:57 -0300

    Clean up init-perl a bit

--- lisp/init-perl.el
+++ lisp/init-perl.el
@@ -5,28 +5,30 @@
 (setup cperl-mode
   (:override-major-mode perl-mode)
 
-  (:documentation-handler cperl-perldoc)
-  (:face cperl-array-face ((t (:inherit font-lock-variable-name-face
-                               :foreground "#57aff6"
-                               :weight bold)))
-         cperl-hash-face ((t (:inherit font-lock-variable-name-face
-                              :foreground "#ff656a"
-                              :slant italic
-                              :weight bold))))
-
   ;; Remap `cperl-indent-command' to `indent-for-tab-command', which
   ;; respects the value of `tab-always-indent'.
   (:bind [remap cperl-indent-command] indent-for-tab-command)
+
+  (:localleader "o" flymake-perlcritic-browse-policy)
+  (:face cperl-array-face ((t (:inherit font-lock-variable-name-face :foreground "#57aff6" :weight bold)))
+         cperl-hash-face ((t (:inherit font-lock-variable-name-face :foreground "#ff656a" :slant italic :weight bold))))
 
   (:when-loaded
     (let ((indent-level 4))
       (:option cperl-close-paren-offset (- indent-level)
                cperl-continued-statement-offset indent-level
-               cperl-electric-keywords t
                cperl-file-style "PBP"
                cperl-indent-level indent-level
                cperl-indent-parens-as-block t
-               cperl-invalid-face 'default))))
+               cperl-invalid-face 'default)
+      (:local-set (remove flymake-diagnostic-functions) 'perl-flymake))))
+
+(setup flymake-perlcritic
+  (:autoload flymake-perlcritic-setup)
+  (:with-mode (perl-mode cperl-mode)
+    (:hook flymake-perlcritic-setup))
+  (:when-loaded
+    (:option flymake-perlcritic-severity 1)))
 
 (setup reply
   (:commands run-reply))
