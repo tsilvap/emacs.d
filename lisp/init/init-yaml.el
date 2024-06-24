af9e3eaf390402aa50eed3a472c02521d33eeb29
Thiago da Silva <thiagodasilva@protonmail.com>
2024-06-24 12:43:32 -0300

    Add initial configuration for Ansible mode

--- lisp/init/init-yaml.el
+++ lisp/init/init-yaml.el
@@ -18,5 +18,34 @@
   (:hook (lambda ()
            (setq-local indent-line-function #'yaml-indent-line))))
 
+;;; Ansible
+
+(setup ansible
+  (:with-map ansible-key-map
+    (:documentation-handler tsp/ansible-browse-documentation))
+  (:with-hook ansible-hook
+    (:hook #'ansible-remove-font-lock)))
+
+(defun +ansible-bounds-of-rule-at-point ()
+  "Return a cons cell containing the start and end of the Ansible rule at point."
+  (let* ((allowed-chars "[:alpha:].")
+         (beg (save-excursion
+                (skip-chars-backward allowed-chars)
+                (point)))
+         (end (save-excursion
+                (skip-chars-forward allowed-chars)
+                (point))))
+    (when (string-match-p "[[:alpha:].]" (buffer-substring beg end))
+      (cons beg end))))
+
+(put 'ansible-rule 'bounds-of-thing-at-point #'+ansible-bounds-of-rule-at-point)
+
+(defun tsp/ansible-browse-documentation (rule)
+  "Open the documentation page of an Ansible RULE."
+  (interactive (list (thing-at-point 'ansible-rule t)))
+  (browse-url
+   (format "https://docs.ansible.com/ansible/latest/collections/%s_module.html"
+           (string-replace "." "/" rule))))
+
 (provide 'init-yaml)
 ;;; init-yaml.el ends here
