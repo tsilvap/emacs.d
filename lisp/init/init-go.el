c1624dc01930c415326014b3597454107c233fd4
Thiago da Silva <thiagodasilva@protonmail.com>
2024-06-24 12:44:35 -0300

    Configure godoc functions in go-mode

--- lisp/init/init-go.el
+++ lisp/init/init-go.el
@@ -3,11 +3,16 @@
 ;;; Code:
 
 (setup go-mode
+  (:bind "C-c c k" godoc-at-point)
   (:localleader "p" go-playground)
   (:hook apheleia-mode
          eglot-ensure)
   (:local-set apheleia-formatter 'goimports
-              tab-width 4))
+              tab-width 4)
+  (:when-loaded
+    (:option godoc-at-point-function 'godoc-gogetdoc
+             godoc-reuse-buffer t
+             godoc-use-completing-read t)))
 
 (setup go-playground
   (:localleader "r" go-playground-rm))
