79059716d8288ba97891513a5d5907df2d88bd42
Thiago da Silva Pinto <thiagodasilva@protonmail.com>
2024-09-18 20:05:55 -0300

    Update tree-sitter configuration

--- lisp/init/init-treesit.el
+++ lisp/init/init-treesit.el
@@ -24,9 +24,33 @@
 (setup prog-mode
   (:hook +treesit-remove-sexp-changes))
 
+;;; Taken from the README of: https://github.com/mickeynp/combobulate
+(defun +treesit-setup-install-grammars ()
+  "Install Tree-sitter grammars if they are absent."
+  (interactive)
+  (dolist (grammar
+           '((css . ("https://github.com/tree-sitter/tree-sitter-css" "v0.20.0"))
+             (html . ("https://github.com/tree-sitter/tree-sitter-html" "v0.20.1"))
+             (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "v0.20.1" "src"))
+             (json . ("https://github.com/tree-sitter/tree-sitter-json" "v0.20.2"))
+             (python . ("https://github.com/tree-sitter/tree-sitter-python" "v0.20.4"))
+             (toml "https://github.com/tree-sitter/tree-sitter-toml")
+             (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "tsx/src"))
+             (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
+             (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))))
+    (add-to-list 'treesit-language-source-alist grammar)
+
+    ;; Only install `grammar' if we don't already have it installed.
+    ;; However, if you want to *update* a grammar then this obviously
+    ;; prevents that from happening.
+    (unless (+treesit-language-available-p (car grammar))
+      (treesit-install-language-grammar (car grammar)))))
+
 (setup combobulate
   (:commands combobulate-mode)
-  (:hook-into yaml-ts-mode))
+  (:hook-into yaml-ts-mode)
+  (:when-loaded
+    (+treesit-setup-install-grammars)))
 
 (provide 'init-treesit)
 ;;; init-treesit.el ends here
